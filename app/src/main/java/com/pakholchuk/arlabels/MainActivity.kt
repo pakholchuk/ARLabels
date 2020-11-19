f123a4f97ca10c43b779a1b8f78f75f7d612ae0d
pakholchuk <r.pakholchuk@gmail.com>
2020-11-19 13:48:38 +0300

    init

---
+++ app/src/main/java/com/pakholchuk/arlabels/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.pakholchuk.arlabels
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.ui.tooling.preview.Preview
+import com.pakholchuk.arlabels.databinding.ActivityMainBinding
+import com.pakholchuk.arlabels.ui.ARLabelsTheme
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val REQUEST_CODE_PERMISSIONS = 10
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ARLabelsTheme {
+        Greeting("Android")
+    }
+}
